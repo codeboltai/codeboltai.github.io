534b2b8f2810058ba5eb2363f554ea0d0e791082
faisal004 <faisalhusain1320@gmail.com>
2024-05-11 13:54:09 +0530

    css added

--- src/components/CBBaseInfo.jsx
+++ src/components/CBBaseInfo.jsx
@@ -1,38 +1,34 @@
 /**
  * This is a custom Front Matter Component that takes the front matter and renders the api data
  */
-import React from 'react';
-import useFrontMatter from './useFrontMatter';
+import React from "react";
+import useFrontMatter from "./useFrontMatter";
 
 const CBBaseInfo = () => {
   const frontMatter = useFrontMatter();
-  console.log(frontMatter)
+  console.log(frontMatter);
   return (
-  <div>
-    {Object.entries(frontMatter).map(([key, value]) => {
-      if (key === 'cbbaseinfo') {
-        console.log(`Key: ${key}, Value: ${value}`);
-        
-        return (
-          <p key={key}>
-            Description: {value.description}
-          </p>
-        );
-        // return Object.entries(value).map(([cbkey, cbvalue]) => {
-        //   console.log(cbkey);
-        //   return (
-        //     <p key={cbkey}>
-        //       <strong>{cbkey}</strong>
-        //       <br/>
-        //     </p>
-        //   );
-        // });
-      }
-      
-      return null;
-    })}
-  </div>
-);
-  };
+    <div>
+      {Object.entries(frontMatter).map(([key, value]) => {
+        if (key === "cbbaseinfo") {
+          console.log(`Key: ${key}, Value: ${value}`);
+
+          return <div className="description" key={key}>Description: {value.description}</div>;
+          // return Object.entries(value).map(([cbkey, cbvalue]) => {
+          //   console.log(cbkey);
+          //   return (
+          //     <p key={cbkey}>
+          //       <strong>{cbkey}</strong>
+          //       <br/>
+          //     </p>
+          //   );
+          // });
+        }
+
+        return null;
+      })}
+    </div>
+  );
+};
 
 export default CBBaseInfo;
--- src/components/CBParameters.jsx
+++ src/components/CBParameters.jsx
@@ -1,31 +1,68 @@
-/**
- * This is a custom Front Matter Component that takes the front matter and renders the api data
- */
-import React from 'react';
-import useFrontMatter from './useFrontMatter';
+import React from "react";
+import useFrontMatter from "./useFrontMatter";
 
 const CBParameters = () => {
   const frontMatter = useFrontMatter();
+
   return (
-  <div>
-    {Object.entries(frontMatter).map(([key, value]) => {
-      if (key === 'cbparameters') {  
-        if (value.parameters) {
-          return value.parameters.map((param, index) => {
-            console.log("Parameter"+param);
-            return (
-              <p key={index}>
-                <strong>{param}</strong>
-                <br/>
-              </p>
-            );
-          });
+    <div>
+      {Object.entries(frontMatter).map(([key, value]) => {
+        if (key === "cbparameters") {
+          const hasParameters = value.parameters && value.parameters.length > 0;
+          const hasReturns = value.returns && value.returns.signatureTypeName && value.returns.description;
+
+          return (
+            <div>
+              {hasParameters && (
+                <div>
+                  <h3>Parameters</h3>
+                  <table className="table" >
+                    <thead>
+                      <tr>
+                        <th>Name</th>
+                        <th>Type</th>
+                        <th>Description</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {value.parameters.map((param, index) => (
+                        <tr key={index}>
+                          <td>{param.name}</td>
+                          <td>{param.typeName}</td>
+                          <td>{param.description}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              )}
+
+              {hasReturns && (
+                <div>
+                  <h3>Returns</h3>
+                  <table className="table" >
+                    <thead>
+                      <tr>
+                        <th>Type</th>
+                        <th>Description</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>{value.returns.signatureTypeName}</td>
+                        <td>{value.returns.description}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              )}
+            </div>
+          );
         }
-      }
-      return null;
-    })}
-  </div>
-);
-  };
+        return null;
+      })}
+    </div>
+  );
+};
 
 export default CBParameters;
