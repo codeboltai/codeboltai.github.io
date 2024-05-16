070d4b2596b6161ac9c778a44a70ab8d537200d0
faisal004 <faisalhusain1320@gmail.com>
2024-05-16 18:01:33 +0530

    changes

--- src/components/CBParameters.jsx
+++ src/components/CBParameters.jsx
@@ -3,20 +3,48 @@ import useFrontMatter from "./useFrontMatter";
 
 const CBParameters = () => {
   const frontMatter = useFrontMatter();
+  const formatReturnType = (returns) => {
+    if (!returns || !returns.signatureTypeName) return "";
+
+    if (returns.typeArgs && returns.typeArgs.length > 0) {
+      const typeArg = returns.typeArgs[0];
+      switch (typeArg.type) {
+        case "array":
+          return "[ ]";
+        case "void":
+          return "void";
+          case "intrinsic":
+          return "";
+          case "reference":
+            return "";
+        default:
+          return `${returns.signatureTypeName}<${typeArg.name}>`;
+      }
+    } else {
+      // Handle no typeArgs or unsupported types
+      return returns.signatureTypeName;
+    }
+  };
 
   return (
     <div>
       {Object.entries(frontMatter).map(([key, value]) => {
         if (key === "cbparameters") {
           const hasParameters = value.parameters && value.parameters.length > 0;
-          const hasReturns = value.returns && value.returns.signatureTypeName && value.returns.description;
+          const hasReturns =
+            value.returns &&
+            value.returns.signatureTypeName &&
+            value.returns.description &&
+            value.returns.typeArgs &&
+            value.returns.typeArgs.length > 0 &&
+            value.returns.typeArgs[0].name;
 
           return (
             <div>
               {hasParameters && (
                 <div>
                   <h3>Parameters</h3>
-                  <table className="table" >
+                  <table className="table">
                     <thead>
                       <tr>
                         <th>Name</th>
@@ -39,21 +67,15 @@ const CBParameters = () => {
 
               {hasReturns && (
                 <div>
-                  <h3>Returns</h3>
-                  <table className="table" >
-                    <thead>
-                      <tr>
-                        <th>Type</th>
-                        <th>Description</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>{value.returns.signatureTypeName}</td>
-                        <td>{value.returns.description}</td>
-                      </tr>
-                    </tbody>
-                  </table>
+                  <h3>Returns:</h3>
+                  <div className="description">
+                    {" "}
+                    {`${value.returns.signatureTypeName}<${
+                      value.returns.typeArgs[0].name || ""
+                    }${formatReturnType(value.returns)}>`}
+                  </div>
+
+                  {value.returns.description}
                 </div>
               )}
             </div>
